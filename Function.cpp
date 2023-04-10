0ce53515d80a1349b0796921706b146822768e35
subeenrange410 <ohdadatewww410@gmail.com>
2023-04-10 10:46:55 +0700

    Chinh sua lai file HelloWorld va them moi file function

---
+++ Function.cpp
@@ -0,0 +1,5 @@
+#include<math.h>
+
+int functionOne(int a, int b){
+    return a+b;
+}
---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    printf("Hello Dat");
+    int x=6;
+    int y=9;
+    int t=functionOne(x,y);
+}
